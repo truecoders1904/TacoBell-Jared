25d9e343b81a5f4931bfe1bc861b48926f785b35
Jared Clark <jmjclark79@gmail.com>
2019-05-14 14:47:56 -0500

    Added tests

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -13,14 +13,33 @@ namespace LoggingKata.Test
 
         [Theory]
         [InlineData("Example")]
-        public void ShouldParse(string str)
+        [InlineData("50, 50, Taco Bell Warrior", null)]
+        [InlineData("0, 180, Taco Bell Gardendale", null)]
+        [InlineData("90, 0, Taco Bell Cullman", null)]
+        public void ShouldParse(string str, ITrackable expected)
         {
-            // TODO: Complete Should Parse
+            // Arrange
+            TacoParser tacoparser = new TacoParser();
+
+            // Act
+            ITrackable actual = tacoparser.Parse(str);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
         [InlineData(null, null)]
         [InlineData("", null)]
+        [InlineData("-1, 50, Taco Bell Warrior", null)]
+        [InlineData("50, -1, Taco Bell Warrior", null)]
+        [InlineData("91, 50, Taco Bell Warrior", null)]
+        [InlineData("50, 181, Taco Bell Warrior", null)]
+        [InlineData("50, 50,", null)]
+        [InlineData(", 50, Taco Bell Warrior", null)]
+        [InlineData("50,, Taco Bell Warrior", null)]
+        [InlineData("taco, 50, Taco Bell Warrior", null)]
+        [InlineData("50, bell, Taco Bell Warrior", null)]
         public void ShouldFailParse(string str, ITrackable expected)
         {
             // Arrange
